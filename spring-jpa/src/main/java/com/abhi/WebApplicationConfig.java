0acd9bfd04c0193755f2cd78a4ea72275b2ffc10
Abhi Bhowmick <cyberabhi2004@yahoo.com>
2017-02-22 20:17:54 +0530

    Latest Change with comments in file

--- spring-jpa/src/main/java/com/abhi/WebApplicationConfig.java
+++ spring-jpa/src/main/java/com/abhi/WebApplicationConfig.java
@@ -1,24 +1,8 @@
 package com.abhi;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Configuration
-public class WebApplicationConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
-
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return null;
-	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return null;
-	}
-
-	@Override
-	protected String[] getServletMappings() {
-		return null;
-	}
-	
+public class WebApplicationConfig {
+	//extends AbstractAnnotationConfigDispatcherServletInitializer{---- Conflict with XML Driven DispatcherServlet Configuration
 }
