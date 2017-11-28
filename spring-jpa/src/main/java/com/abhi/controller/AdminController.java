3fa0c4c1ff91be3b5174e3d6ebb4cee5e0fb4811
Abhi Bhowmick <cyberabhi2004@yahoo.com>
2017-11-28 15:28:36 +0530

    Added changes for Main app on Https and Mgmt app on http

--- spring-jpa/src/main/java/com/abhi/controller/AdminController.java
+++ spring-jpa/src/main/java/com/abhi/controller/AdminController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.abhi.audit.service.AuditService;
-
 import reactor.bus.Event;
 import reactor.bus.EventBus;
 
