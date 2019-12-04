977caa34b48bc9f2d495e2b4aace42e6b067b4d1
Abhi <cyberabhi2004@yahoo.com>
2019-12-04 17:28:08 +0530

    Added logger

--- spring-boot-redis-demo/src/main/java/com/abhi/redis/demo/StudentController.java
+++ spring-boot-redis-demo/src/main/java/com/abhi/redis/demo/StudentController.java
@@ -2,6 +2,8 @@ package com.abhi.redis.demo;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,16 +14,21 @@ import com.abhi.redis.service.StudentService;
 
 @RestController
 public class StudentController {
+	private Logger logger = LoggerFactory.getLogger(StudentController.class);
+	
 	@Autowired
 	private StudentService studentService;
 	
 	@GetMapping("/students")
 	public List<Student> findAll() {
-		return studentService.findAll();		
+		List<Student> list = studentService.findAll();
+		logger.debug("StudentController :: "+list.size() );
+		return list;		
 	}
 	
 	@GetMapping("/students/{id}")
 	public Student findOne(@PathVariable Long id) {
+		logger.debug("StudentController/{id} id :: "+id);
 		return studentService.findById(id)
 	      .orElseThrow(() -> new RuntimeException());
 	}
