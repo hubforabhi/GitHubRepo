f93edd82c645f4ee98a3edad091b4252931090fb
Abhi <cyberabhi2004@yahoo.com>
2020-02-19 08:36:49 +0530

    Adding Method Security

--- spring-boot-security-demo/src/main/java/com/abhi/spring/security/demo/controller/AdminController.java
+++ spring-boot-security-demo/src/main/java/com/abhi/spring/security/demo/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class AdminController {
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 	
 	@GetMapping("/ping")
+	@PreAuthorize(value = "hasRole('ADMIN')")
 	public ResponseEntity<HttpStatus> ping() {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
--- spring-boot-security-demo/src/main/java/com/abhi/spring/security/demo/security/JwtUtils.java
+++ spring-boot-security-demo/src/main/java/com/abhi/spring/security/demo/security/JwtUtils.java
@@ -1,6 +1,7 @@
 package com.abhi.spring.security.demo.security;
 
 import java.util.Date;
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
@@ -41,7 +43,7 @@ public class JwtUtils {
 	public String getUserNameFromJwtToken(String token) {
 		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
 	}
-
+	
 	public boolean validateJwtToken(String authToken) {
 		try {
 			logger.debug("JwtUtils.validateJwtToken authToken "+authToken);
@@ -60,5 +62,26 @@ public class JwtUtils {
 		}
 
 		return false;
+	}
+	
+	//retrieve expiration date from jwt token
+	public Date getExpirationDateFromToken(String token) {
+		return getClaimFromToken(token, Claims::getExpiration);
+	}
+	
+	public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = getAllClaimsFromToken(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	//for retrieveing any information from token we will need the secret key
+	private Claims getAllClaimsFromToken(String token) {
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+	}	
+	
+	//check if the token has expired
+	public Boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		return expiration.before(new Date());
 	}
 }
